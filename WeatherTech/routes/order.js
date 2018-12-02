037c9bbda337e352fecb67590c9440b4d78ef2a1
Sam Pyeatt <sampyeatt@gmail.com>
2018-12-02 16:13:46 -0700

    added order.js

---
+++ WeatherTech/routes/order.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var db = require('../database');
+var app = express();
+module.exports = app;
+
+//needs the order page to finish
+
+app.post('/order', function(request, response){
+	db.none('INSERT INTO activeorders(username, item) VALUES($1, $2)', [item.Name, item.item])
+	.then(function (result){
+		request.flash('success', 'Order placed');
+		response.render('index',{})
+		.catch(function (err){
+			request.flash('error', 'Please enter valid information');
+			response.render(/*path to same page*/,{})
+		});
+	}
+})
